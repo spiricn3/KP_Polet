0125d1be4a2ee5d24208c1ff1adf07b76a05760d
Neda Spiric <147815549+spiricn3@users.noreply.github.com>
2024-06-11 13:13:37 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+  window.onload = function() {
+    
+    var opisi = document.querySelectorAll('.opis p');
+    opisi.forEach(function(opis) {
+      opis.style.display = 'none';
+    });
+   
+    var prviOpis = document.getElementById('djelatnost1');
+    prviOpis.style.display = 'block';
+  };
+  
+  function prikaziOpis(djelatnostId, event) {
+    event.preventDefault(); 
+    
+    var opisi = document.querySelectorAll('.opis p');
+    opisi.forEach(function(opis) {
+      opis.style.display = 'none';
+    });
+    
+    var odabraniOpis = document.getElementById(djelatnostId);
+    odabraniOpis.style.display = 'block';
+  }
+
